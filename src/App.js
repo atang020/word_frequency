41b2e46afa89dbeb8a8af139d1bb1586bc0828e2
Alex Tang <alex.tang.dev@gmail.com>
2022-01-17 19:13:49 -0800

    chore: add error boundary and event error handling for empty string content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
+import ErrorBoundary from './components/error/ErrorBoundary';
 import './App.css';
 
 function App() {
   const [content, setContent] = useState('');
   const [wordMap, setWordMap] = useState({});
   const [sortedKeys, setSortedKeys] = useState([]);
+  const [error, setError] = useState()
 
   const generateMapping = () => {
+    if (!content) {
+      setError("Cannot map an empty content. Please provide text content");
+      return;
+    }
     const array = content.toLowerCase().match(/\w+(?:'\w+)*/g);
     let map = {};
     for (let a of array) {
@@ -24,28 +30,31 @@ function App() {
   }
 
   return (
-    <div className="App">
-        <textarea onBlur={(e) => { setContent(e.target.value)}} rows="50"></textarea>
-        <button onClick={() => { generateMapping()}}>Get words frequency</button>
-        <table>
-          <thead>
-            <tr>
-              <th>Word</th>
-              <th>Count</th>
-            </tr>
-          </thead>
-          <tbody>
-            {sortedKeys.map((val, idx) => {
-              return (
-                <tr key={val}>
-                  <td>{val}</td>
-                  <td>{wordMap[val]}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-    </div>
+    <ErrorBoundary>
+      <div className="App">
+          <textarea onBlur={(e) => { setContent(e.target.value)}} rows="20"></textarea>
+          <button onClick={() => { generateMapping()}}>Get words frequency</button>
+          {error && <div>{error}</div>}
+          <table>
+            <thead>
+              <tr>
+                <th>Word</th>
+                <th>Count</th>
+              </tr>
+            </thead>
+            <tbody>
+              {sortedKeys.map((val, idx) => {
+                return (
+                  <tr key={val}>
+                    <td>{val}</td>
+                    <td>{wordMap[val]}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+      </div>
+    </ErrorBoundary>
   );
 }
 
